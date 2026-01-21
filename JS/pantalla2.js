a25ab4d5582f2b6dadffaa745c6d3a405f3d8f1f
yolanda <yogomgon2014@gmail.com>
2026-01-21 16:22:23 -0500

    se agrega la leyenda

--- JS/pantalla2.js
+++ JS/pantalla2.js
@@ -7,11 +7,27 @@ console.log ("entro a pantalla 2")
   const inputNombre = document.getElementById("nombreAvatar");
 
 
-  const btnGuardar = document.getElementById("guardar");
+  const btnGuardar = document.getElementById("guardar_continuar");
 
 btnGuardar.addEventListener("click", () => {
     console.log(inputNombre)
   const valor = inputNombre.value;
 
   localStorage.setItem("nombre", valor);
+   window.location.href = "./pantalla3.html";
+});
+
+const btnLeyenda = document.getElementById("btnLeyenda");
+const divLeyenda = document.getElementById("leyenda");
+
+btnLeyenda.addEventListener("click", () => {
+  // Alternar la visibilidad del div
+  divLeyenda.classList.toggle("oculto");
+  
+  // Cambiar el texto del botón según el estado
+  if (divLeyenda.classList.contains("oculto")) {
+    btnLeyenda.textContent = "Leyenda";
+  } else {
+    btnLeyenda.textContent = "Ocultar Leyenda";
+  }
 });
